dcc99c474e85f333f1b45d8bb7374687f885d0fe
andreiv <Andrei.Varzari@netapp.com>
2018-01-11 19:40:17 +0200

    Add Template pattern

---
+++ Template/app/template/house/app/Main.java
@@ -0,0 +1,14 @@
+package template.house.app;
+
+public class Main {
+
+    public static void main(String[] args) {
+        HouseTemplate houseType = new WoodenHouse();
+        houseType.buildHouse();
+
+        System.out.println("************");
+
+        houseType = new GlassHouse();
+        houseType.buildHouse();
+    }
+}
--- Template/app/template/xxx/app/Main.java
+++
@@ -1,8 +0,0 @@
-package template.xxx.app;
-
-public class Main {
-
-    public static void main(String[] args) {
-
-    }
-}
